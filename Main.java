e86946875e872ef6bfc6592ce025c7f6953587a9
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-10-24 14:50:35 +0200

    Poprawiona wersja ONP

--- Main.java
+++ Main.java
@@ -1,20 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        ONP calc = new ONP("2 7 + 3 / 14 3 - 4 * + 2 /");
-        System.out.println(calc.evaluate());
-
-        /*Stack stos = new Stack();
-        for (int i = 0; i < 20; ++i) {
-            stos.put(i);
-        }
-        try {
-           while(true){                          //powtarzamy do momentu wyrzucenia wyjątku
-                System.out.println(stos.pop().getValue());
-            }
-        }catch(EmptyStackException ex){         //przyjmujemy tylko konkretny wyjątek końca stosu
-            System.out.println(ex.getMessage());
-        }
-*/
+        ONP calc = new ONP();
+        calc.oblicz("2 7 + 3 / 14 3 - 4 * + 2 /");
+        System.out.println("----------------------");
+        calc.oblicz("6 14 * 56 + 23 4 * 78 : - 97 /");
+        System.out.println("----------------------");
+        calc.oblicz("6 * 56 + 23 4 * 78 : - 97 /");  //przykład celowo błędny- na potrzeby testów
+        System.out.println("----------------------");
+        calc.oblicz("7 13 + 25 - 23 4 / 5 * + 13 /");
+        System.out.println("----------------------");
+        calc.oblicz("7 13 25 23 4 18 13");  //inny przykad niepoprawnej składni - tutaj błąd nie jest komunikowany, a podawany jest ostatni element stosu i jest to zachowanie prawiodłowe
+        System.out.println("----------------------");
+        calc.oblicz("!@$%!@##$%@$$^##@$FSDERV#F$G%~~~~"); //niepoprawna składnia- użyto niedozwolonych znaków
     }
 }
--- ONP.java
+++ ONP.java
@@ -5,43 +5,61 @@ public class ONP {
 
     public ONP(String text) {
         this.text = text;
-        this.cutText = text.split(" ");
     }
 
-    public double evaluate() {
+    public ONP(){
+
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void oblicz() {
+        this.cutText = text.split(" ");
         for (int i = 0; i < cutText.length; i++) {
-            String s = cutText[i];
+            String s = cutText[i];      //tablica z pociętym tekstem wejściowym
             try {
-                double d;
-               // System.out.println(s);
-                if (s.equals("x")||s.equals("*")) {
-                   stos.put(stos.pop().getValue()* stos.pop().getValue());
-                }else if(s.equals(":")||s.equals("/")) {
-                    double a= stos.pop().getValue();
-                    double b= stos.pop().getValue();
-                    stos.put((double)(b/a));
-                }else if (s.equals("+")) {
+                double a, b;        //zmienne pomocnicze do dzielenia i odejmowania
+                if (s.equals("x") || s.equals("*")) {   //mnożenie
+                    stos.put(stos.pop().getValue() * stos.pop().getValue());
+                } else if (s.equals(":") || s.equals("/")) {     //dzielenie
+                    a = stos.pop().getValue();
+                    b = stos.pop().getValue();
+                    stos.put((double) (b / a));
+                } else if (s.equals("+")) {             //dodawanie
                     stos.put(stos.pop().getValue() + stos.pop().getValue());
-                }else if (s.equals("-")) {
-                    double a= stos.pop().getValue();
-                    double b= stos.pop().getValue();
-                    stos.put(b-a);
-                }else {
-                   stos.put(Double.parseDouble(s));
+                } else if (s.equals("-")) {             //odejmowanie
+                    a = stos.pop().getValue();
+                    b = stos.pop().getValue();
+                    stos.put(b - a);
+                } else {
+                    try {
+                        stos.put(Double.parseDouble(s));
+                    } catch (NumberFormatException ex) {    //błąd przy parsowaniu na doubla- jeśli napotkamy na nie-liczbę
+                        System.out.println(text+ " = " +"Wpisano znak niedozwolony- dozwolone są tylko liczby i znaki operacji");
+                        break;
+                    }
                 }
-            } catch (EmptyStackException ex) {
-                System.out.println("znak. a nie ma liczb");
+            } catch (EmptyStackException ex) {//błąd gdy stos liczb jest pusty
+                System.out.println(text + " = " + "ZLA SKLADNIA ZA MAO LICZB ZA DUZO OPERACJI");
+                break;
             }
-           // System.out.println(this.stos.toString());
+            //System.out.println(this.stos.toString());
         }
         try {
-            return stos.pop().getValue();
-        }catch (EmptyStackException ex){
-            System.out.println("nie ma wyniku");
+            System.out.println(text + " = " + stos.pop().getValue());
+        } catch (EmptyStackException ex) {
+
         }
-        return -1;
     }
 
+    public void oblicz(String s) {   //opcja wyliczania zadanego tekstu bez potrzeby tworzenia nowego kalkulatora.
+        this.text = s;
+        this.stos = new Stack();
+        //this.cutText = null;
+        oblicz();
+    }
 
 }
 
